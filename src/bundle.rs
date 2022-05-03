7dda7cfd39b5ffb443bdc60e31fb196027e2b24b
Jorge Prendes <jorge.prendes@gmail.com>
2022-05-03 12:28:29 +0100

    Use a custom type to store bundling result

--- src/bundle.rs
+++ src/bundle.rs
@@ -1,12 +1,52 @@
 use syn_inline_mod::{Error as InlineError, InlinerBuilder};
+use std::io::Write;
 use std::path::Path;
+use std::process::{Command, Stdio};
+use quote::ToTokens;
 use crate::error::MultiError;
 
-pub fn bundle_file(src: &Path) -> Result<String, MultiError> {
-    match InlinerBuilder::new().parse_and_inline_modules(src) {
-        Ok(f) if f.has_errors() == false => Ok(prettyplease::unparse(&f.into_output_and_errors().0)),
-        Ok(f) => Err(MultiError::from_inline_errors(f.errors())),
-        Err(InlineError::Io(err)) => Err(MultiError::from_io_error(src, &err)),
-        Err(InlineError::Parse(err)) => Err(MultiError::from_parse_error(src, &err)),
+pub struct Bundle(syn::File);
+
+impl Bundle {
+    pub fn new(src: &Path) -> Result<Bundle, MultiError> {
+        match InlinerBuilder::new().parse_and_inline_modules(src) {
+            Ok(f) if f.has_errors() == false => Ok(Bundle(f.into_output_and_errors().0)),
+            Ok(f) => Err(MultiError::from_inline_errors(f.errors())),
+            Err(InlineError::Io(err)) => Err(MultiError::from_io_error(src, &err)),
+            Err(InlineError::Parse(err)) => Err(MultiError::from_parse_error(src, &err)),
+        }
     }
+
+    pub fn minify(&self) -> String {
+        let input = &self.0;
+        input.into_token_stream().to_string()
+    }
+
+    pub fn prettify(&self) -> String {
+        prettify_prettyplease(&self.0)
+            .unwrap_or_else(|_| {
+                let ugly = self.minify();
+                prettify_rustfmt(&ugly).unwrap_or(ugly)
+            })
+    }
+}
+
+fn prettify_rustfmt(input: &str) -> anyhow::Result<String> {
+    let mut child = Command::new("rustfmt")
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped())
+        .stdin(Stdio::piped())
+        .spawn()?;
+    child.stdin.take().unwrap().write_all(input.as_bytes())?;
+    let output = child.wait_with_output()?;
+    if !output.status.success() {
+        anyhow::bail!("{}", String::from_utf8(output.stderr)?)
+    }
+    Ok(String::from_utf8(output.stdout)?)
+}
+
+fn prettify_prettyplease(input: &syn::File) -> std::thread::Result<String> {
+    std::panic::catch_unwind(|| {
+        prettyplease::unparse(input)
+    })
 }
--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,18 @@ use clap::Parser;
 pub mod bundle;
 pub mod error;
 
+use bundle::Bundle;
+
 /// Bundle the content of a crate into a single rust source file
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Args {
     /// Path of the entry file
     entry: PathBuf,
+
+    /// Do not prettify output
+    #[clap(short, long)]
+    minify: bool
 }
 
 fn parse_args() -> Args {
@@ -22,10 +28,19 @@ fn parse_args() -> Args {
     }))
 }
 
+fn main_inner(args: Args) -> Result<String, error::MultiError> {
+    let bundled = Bundle::new(args.entry.as_path())?;
+    if args.minify {
+        Ok(bundled.minify())
+    } else {
+        Ok(bundled.prettify())
+    }
+}
+
 fn main() {
     let args = parse_args();
-    match bundle::bundle_file(args.entry.as_path()) {
+    match main_inner(args) {
         Ok(output) => println!("{}", output),
-        Err(error) => eprintln!("cargo-bundle error:\n\n{}", error),
+        Err(error) => eprintln!("cargo-bundle error:\n\n{}", error),        
     }
 }
